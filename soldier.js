d20e29dbd9349a27069adaa4884b7c673d5d363c
Denzoll <elikhanovden@gmail.com>
2022-05-18 10:48:22 +0300

    start

---
+++ soldier.js
@@ -0,0 +1,13 @@
+const soldier = {
+    name: 'Soup',
+    health: 10,
+    weapon:{
+        name: 'm416',
+        ammo: 30
+    },
+    supply: 3,
+    fire: function(){},
+    reload: function(){},
+    hurt: function(){},
+
+}
